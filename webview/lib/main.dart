991e7e5b4f8ba31260146167033bea4c266519c7
Ali Othman <eg.aliothman@gmail.com>
2023-07-01 16:34:07 +0300

    On branch main  Changes to be committed: to force the app to open any external URL in  google browser 	modified:   webview/lib/main.dart 	modified:   webview/linux/flutter/generated_plugin_registrant.cc 	modified:   webview/linux/flutter/generated_plugins.cmake 	modified:   webview/macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   webview/pubspec.lock 	modified:   webview/pubspec.yaml 	modified:   webview/windows/flutter/generated_plugin_registrant.cc 	modified:   webview/windows/flutter/generated_plugins.cmake

--- webview/lib/main.dart
+++ webview/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -9,13 +10,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final String url = 'http://80.211.131.112:1880/ui';
+
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( 
-        child :WebView(
-        initialUrl: url,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: SafeArea(
+        child: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+          navigationDelegate: (NavigationRequest request) {
+            if (!request.url.startsWith('http://80.211.131.112:1880/ui')) {
+              _launchURL(request.url); // Open external links in the Google app
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
         ),
       ),
     );
